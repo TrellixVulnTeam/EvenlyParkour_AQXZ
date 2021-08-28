c16b82a1a5c70d301ccddc7f5e78492b05d0b60d
coderEvenlyPlaying <chris.wang0227@gmail.com>
2021-08-28 18:10:11 +0800

    EvenlyParkour 0.1b1
    Official beta release for EvenlyParkour! Enjoy!

--- 3.0/scripts/modules/bl_keymap_utils/__init__.py
+++ 3.0/scripts/modules/bl_keymap_utils/__init__.py
@@ -1,3 +1,25 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:0d92c59934d0bef8afe86929a4044f4a2e7d741d479e5dfb386fb24b9e29a588
-size 915
+# ##### BEGIN GPL LICENSE BLOCK #####
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# ##### END GPL LICENSE BLOCK #####
+
+# <pep8 compliant>
+
+__all__ = (
+    "io",
+    "keymap_from_toolbar",
+    "keymap_hierarchy",
+)
--- README.md
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:5e029ae565eba9fc54d51912548aedf83f727199d5d0c86623ff2d1342c5c0a0
-size 425
